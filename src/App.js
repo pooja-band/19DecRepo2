821aefdd8886c77810e967363d4ed910b82a5130
Pooja Band <pooja.band@citiustech.com>
2024-12-19 15:21:59 +0530

    Pushing Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Likes from './Likes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Welcome all...</h1>
+      <h2 className="text-center mb-4">Please hit the button below if you like the image.</h2>
+      <Likes value={0}/>
     </div>
   );
 }
